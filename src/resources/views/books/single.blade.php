bb103e844fc8451a43b97c6b173abd5c0bfc60ec
Azhar Uddin <azhar@sender.net>
2023-12-28 19:29:53 +0600

    update

--- src/resources/views/books/single.blade.php
+++ src/resources/views/books/single.blade.php
@@ -11,36 +11,38 @@
         </ul>
     </div>
 @endif
+<div class="mx-auto col-md-8">
 
-<form action="{{ (empty($book->id) ? route('books.store') : route('books.update', $book->id))}}" method="POST">
-    @csrf
-    @if (!empty($book->id))
-        @method('PUT')
-    @endif
-  
-     <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Title:</strong>
-                <input type="text" name="title" class="form-control" placeholder="Title" value="{{ $book->title ?? '' }}">
+    <form action="{{ (empty($book->id) ? route('books.store') : route('books.update', $book->id))}}" method="POST">
+        @csrf
+        @if (!empty($book->id))
+            @method('PUT')
+        @endif
+      
+         <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Title:</strong>
+                    <input type="text" name="title" class="form-control" placeholder="Title" value="{{ $book->title ?? '' }}">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Author:</strong>
+                    <input type="text" name="author" class="form-control" placeholder="Author" value="{{ $book->author ?? '' }}">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Description:</strong>
+                    <textarea class="form-control" style="height:150px" name="description" placeholder="Description">{{ $book->description ?? '' }}</textarea>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                    <button type="submit" class="btn btn-primary">Submit</button>
             </div>
         </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Author:</strong>
-                <input type="text" name="author" class="form-control" placeholder="Author" value="{{ $book->author ?? '' }}">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Description:</strong>
-                <textarea class="form-control" style="height:150px" name="description" placeholder="Description">{{ $book->description ?? '' }}</textarea>
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
-    </div>
-   
-</form>
+       
+    </form>
+</div>
 @endsection
