c34fb98c4afc8ec6384cff26900ab032e778e4cc
Azhar Uddin <azhar@sender.net>
2023-12-28 19:08:09 +0600

    update

--- src/resources/views/books/index.blade.php
+++ src/resources/views/books/index.blade.php
@@ -18,13 +18,6 @@
         <td>{{$book->id}}</td>
         <td>{{$book->title}}</td>
         <td>{{$book->author}}</td>
-        <td><button>Edit</button>
-            <form action="{{ route('books.destroy' , $book->id) }}" method="POST">
-                @method('DELETE')
-                <input type="hidden" value="{{$book->id}}">
-                <input type="submit" placeholder="Delete" class="px-4 py-2 text-white bg-red-500 hover:bg-red-700">
-            </form>
-        </td>
         <td>
             <form action="{{ route('books.destroy',$book->id) }}" method="POST">
                 @csrf
